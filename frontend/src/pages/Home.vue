760730381a8802798e373fb88d91b03307c1843a
Gavin D'souza <gavin18d@gmail.com>
2022-04-11 18:56:48 +0530

    feat(dashboard): Show last refresh time, auto-refresh every minute

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-3xl py-12 mx-auto grid gap-4 grid-cols-2">
-    <Card title="Important" class="col-span-2" style="background-color: rgb(240 253 244);">
+    <Card title="Important" :subtitle=fetch_subtitle() class="col-span-2" style="background-color: rgb(240 253 244);">
       <ListItem
         v-for="notification in important"
         :key="notification.name"
@@ -89,10 +89,14 @@ export default {
       updates: null,
       forums: null,
       promos: null,
+      timer: '',
+      countDown : 60,
     }
   },
   mounted() {
+    this.timer = setInterval(this.fetchNotifications, 60_000);
     this.fetchNotifications();
+    this.countDownTimer();
   },
   methods: {
     async fetchNotifications() {
@@ -100,7 +104,31 @@ export default {
       this.updates = await this.$call('rebalance.api.get', {category: 'Updates'});
       this.forums = await this.$call('rebalance.api.get', {category: 'Forums'});
       this.promos = await this.$call('rebalance.api.get', {category: 'Promotions'});
+      this.countDown = 60;
     },
+    fetch_subtitle() {
+      if (this.countDown == 0) {
+        return `Refreshing dashboard...`;
+      } else if (this.countDown >= 25) {
+        return `Dashboard refreshed`;
+      } else {
+        return `Last refresh ${ 60 - this.countDown } seconds ago`;
+      }
+    },
+    cancelAutoUpdate () {
+        clearInterval(this.timer);
+    },
+    beforeDestroy () {
+      this.cancelAutoUpdate();
+    },
+    countDownTimer() {
+      if (this.countDown > 0) {
+          setTimeout(() => {
+              this.countDown -= 5
+              this.countDownTimer()
+          }, 5000)
+      }
+    }
   },
   components: {
     Dialog,
